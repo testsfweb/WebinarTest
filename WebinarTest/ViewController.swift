4e8cd44ee1122a156ad2a2ff31f88f62815087bc
Kirill Kraynov <kirillkrainovsf@gmail.com>
2022-07-11 21:15:21 +0300

    initial commit

---
+++ WebinarTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebinarTest
+//
+//  Created by Kirill Kraynov on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
